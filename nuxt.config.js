a43748bd9b5c1e2be28eec649e2a8e5aa0461a39
Alex Montgomery <Alexpm@gmail.com>
2017-11-21 14:20:49 +0800

    yarn add @nuxtjs/apollo + add to modules section of config + add client-configs.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,14 @@ module.exports = {
   ** Customize the progress bar color
   */
   loading: { color: '#3B8070' },
+  // Add apollo module
+  modules: ['@nuxtjs/apollo'],
+  // Give apollo module options
+  apollo: {
+    clientConfigs: {
+      default: '~/apollo/client-configs/default.js'
+    }
+  },
   /*
   ** Build configuration
   */
